0d0afcc31ce9719ffe62515398d1c3ab77b11cb7
Alexandra Paramonova <alpar.wb@gmail.com>
2023-12-11 16:27:43 +0500

    feat: add pause for auto slider on touch and mouse events

--- pages/home/scripts/slider.js
+++ pages/home/scripts/slider.js
@@ -4,6 +4,18 @@ const selectBars = document.querySelectorAll(".select-bar");
 const sliderImages = document.querySelectorAll(".slider-item__image");
 const deltaSliderToNext = 1;
 const deltaSliderToPrev = -1;
+let activeSelectBar;
+
+// variables for the Auto Slider
+const sliderSwitchTimeInMs = 6000;
+const oneStepAnimationInMs = 6;
+let currentTimerId;
+let currentTimerStartTime;
+let isAutoSwipePaused = false;
+let swipePauseStartTime;
+let resumeTimerDurationTime;
+let currentStepsAnimation;
+let currentSliderTimeShown;
 
 const buttonSliderPrev = document.querySelector(
   ".slider-favorite__control_previous",
@@ -32,6 +44,8 @@ function removeActiveSelectBar() {
 
 function addActiveSelectBar() {
   selectBars[currentItem].classList.add("select-bar_active");
+  currentStepsAnimation = 0;
+  currentSliderTimeShown = 0;
 }
 
 function swipeSlider(swipeDelta) {
@@ -40,15 +54,29 @@ function swipeSlider(swipeDelta) {
   const nextPosition = getSliderNextPosition(currentItem);
   changeSliderToNextPosition(nextPosition);
   addActiveSelectBar();
+  activeSelectBar = document.querySelector(".select-bar_active");
+}
+
+function autoSwipeSlider() {
+  swipeSlider(deltaSliderToNext);
+  currentTimerId = setTimeout(autoSwipeSlider, sliderSwitchTimeInMs);
+  currentTimerStartTime = Date.now();
+}
+
+function manualSwipeSlider(swipeDelta) {
+  clearInterval(currentTimerId);
+  swipeSlider(swipeDelta);
+  currentTimerId = setTimeout(autoSwipeSlider, sliderSwitchTimeInMs);
+  currentTimerStartTime = Date.now();
 }
 
 // arrow buttons event listeners
 buttonSliderPrev.addEventListener("click", () => {
-  swipeSlider(deltaSliderToPrev);
+  manualSwipeSlider(deltaSliderToPrev);
 });
 
 buttonSliderNext.addEventListener("click", () => {
-  swipeSlider(deltaSliderToNext);
+  manualSwipeSlider(deltaSliderToNext);
 });
 
 // swipe by touch / mouse
@@ -70,45 +98,61 @@ function getSwipePosition(event) {
 }
 
 function swipeStart(event) {
+  event.preventDefault();
   isSwiping = true;
   [swipePositionXStart, swipePositionYStart] = getSwipePosition(event);
   sliderList.style.cursor = "grabbing";
 }
 
 function swipeMove(event) {
+  event.preventDefault();
   if (isSwiping) {
     [swipePositionXEnd, swipePositionYEnd] = getSwipePosition(event);
   }
 }
 
-function swipeEnd() {
+function swipeEnd(event) {
+  event.preventDefault();
   if (isSwiping) {
     sliderList.style.cursor = "grab";
     const swipeDeltaX = swipePositionXEnd - swipePositionXStart;
     const swipeDeltaY = swipePositionYEnd - swipePositionYStart;
-    const shortSwipeXLength = 100;
+    const shortSwipeXLength = 200;
     const longSwipeYLength = 200;
     const isShortSwipeX = Math.abs(swipeDeltaX) > shortSwipeXLength;
     const isLongSwipeY = Math.abs(swipeDeltaY) > longSwipeYLength;
     const isSwipeToNext = swipeDeltaX < 0;
     if (!isLongSwipeY && isShortSwipeX) {
       if (isSwipeToNext) {
-        swipeSlider(deltaSliderToNext);
+        manualSwipeSlider(deltaSliderToNext);
       } else {
-        swipeSlider(deltaSliderToPrev);
+        manualSwipeSlider(deltaSliderToPrev);
       }
     }
   }
   isSwiping = false;
 }
 
-// auto slider
-const sliderSwitchTimeInMs = 6000;
-let currentTimeoutId;
+function pauseAutoSwipe() {
+  isAutoSwipePaused = true;
+  swipePauseStartTime = Date.now();
+  clearTimeout(currentTimerId);
+  activeSelectBar.style.animationPlayState = "paused";
+  currentSliderTimeShown += swipePauseStartTime - currentTimerStartTime;
+  resumeTimerDurationTime = sliderSwitchTimeInMs - currentSliderTimeShown;
+  currentStepsAnimation = (
+    currentSliderTimeShown / oneStepAnimationInMs
+  ).toFixed(0);
+}
 
-function autoSwipeSlider() {
-  swipeSlider(deltaSliderToNext);
-  currentTimeoutId = setTimeout(autoSwipeSlider, sliderSwitchTimeInMs);
+function resumeAutoSwipe() {
+  if (isAutoSwipePaused) {
+    currentTimerId = setTimeout(autoSwipeSlider, resumeTimerDurationTime);
+    currentTimerStartTime = Date.now();
+    activeSelectBar.style.animationTimingFunction = `steps(${currentStepsAnimation}, end)`;
+    activeSelectBar.style.animationPlayState = "running";
+    isAutoSwipePaused = false;
+  }
 }
 
 autoSwipeSlider();
@@ -116,17 +160,26 @@ autoSwipeSlider();
 // Mouse Events
 sliderList.addEventListener("mouseenter", () => {
   sliderList.style.cursor = "grab";
+  pauseAutoSwipe();
 });
 sliderList.addEventListener("mousedown", (e) => swipeStart(e));
-sliderList.addEventListener("mouseup", swipeEnd);
-sliderList.addEventListener("mouseleave", swipeEnd);
+sliderList.addEventListener("mouseup", (e) => swipeEnd(e));
+sliderList.addEventListener("mouseleave", (e) => {
+  resumeAutoSwipe();
+  swipeEnd(e);
+});
 sliderList.addEventListener("mousemove", (e) => swipeMove(e));
 // Touch Events
-sliderList.addEventListener("touchstart", (e) => swipeStart(e), {
-  passive: true,
+sliderList.addEventListener("touchstart", (e) => {
+  pauseAutoSwipe();
+  swipeStart(e);
 });
-sliderList.addEventListener("touchend", swipeEnd);
-sliderList.addEventListener("touchcancel", swipeEnd);
-sliderList.addEventListener("touchmove", (e) => swipeMove(e), {
-  passive: true,
+sliderList.addEventListener("touchend", (e) => {
+  resumeAutoSwipe();
+  swipeEnd(e);
 });
+sliderList.addEventListener("touchcancel", (e) => {
+  resumeAutoSwipe();
+  swipeEnd(e);
+});
+sliderList.addEventListener("touchmove", (e) => swipeMove(e));
